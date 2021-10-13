a086edf4f4a1704f0495a31f64fa7feea13cbeb9
julien <julien@kiboko.fr>
2021-10-13 16:52:49 +0200

    WIP : Hook runtime

---
+++ src/Hook.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kiboko\Component\Runtime\Hook;
+
+use Kiboko\Component\State;
+use Kiboko\Contract\Pipeline\LoaderInterface;
+use Kiboko\Contract\Pipeline\LoadingInterface;
+use Kiboko\Contract\Pipeline\NullState;
+use Kiboko\Contract\Pipeline\PipelineInterface;
+use Kiboko\Contract\Pipeline\RejectionInterface;
+use Kiboko\Contract\Pipeline\StateInterface;
+use Kiboko\Contract\Pipeline\TransformerInterface;
+use Kiboko\Contract\Pipeline\TransformingInterface;
+use Psr\Container\ContainerInterface;
+
+class Hook implements HookRuntimeInterface
+{
+    private StateInterface $state;
+
+    public function __construct(
+        private PipelineInterface $pipeline,
+        private ContainerInterface $container,
+        ?StateInterface $state = null
+    ) {
+        $this->state = $state ?? new NullState();
+    }
+
+    public function transform(
+        TransformerInterface $transformer,
+        RejectionInterface $rejection,
+        StateInterface $state,
+    ): TransformingInterface {
+        $this->pipeline->transform($transformer, $rejection, $state = new State\MemoryState($state));
+
+//        $this->state->withStep('transformer')
+//            ->addMetric('read', $state->observeAccept())
+//            ->addMetric('error', fn() => 0)
+//            ->addMetric('rejected', $state->observeReject());
+
+        return $this;
+    }
+
+    public function load(
+        LoaderInterface $loader,
+        RejectionInterface $rejection,
+        StateInterface $state,
+    ): LoadingInterface {
+        $this->pipeline->load($loader, $rejection, $state = new State\MemoryState($state));
+
+//        $this->state->withStep('loader')
+//            ->addMetric('read', $state->observeAccept())
+//            ->addMetric('error', fn() => 0)
+//            ->addMetric('rejected', $state->observeReject());
+
+        return $this;
+    }
+
+    public function run(): int
+    {
+        $line = 0;
+        foreach ($this->pipeline->walk() as $item) {
+            $line++;
+        }
+//        $this->state->update();
+
+        return $line;
+    }
+
+    public function feed(...$data): HookRuntimeInterface
+    {
+        $this->pipeline->feed(...$data);
+
+        return $this;
+    }
+
+    public function container(): ContainerInterface
+    {
+        return $this->container;
+    }
+}
---
+++ src/HookRuntimeInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kiboko\Component\Runtime\Hook;
+
+use Kiboko\Contract\Pipeline\LoadingInterface;
+use Kiboko\Contract\Pipeline\RunnableInterface;
+use Kiboko\Contract\Pipeline\TransformingInterface;
+use Psr\Container\ContainerInterface;
+
+interface HookRuntimeInterface extends TransformingInterface, LoadingInterface, RunnableInterface
+{
+    public function feed(...$data): self;
+    public function container(): ContainerInterface;
+}
